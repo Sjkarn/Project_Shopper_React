968c513b770c300e57f7cd6e78f12fa603b06f18
Saurabh Karn <saurabhkarn007@gmail.com>
2023-08-07 12:44:54 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ShopperIndex } from './components/shopper-index/shopper-index';
+import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CookiesProvider>
+      <ShopperIndex />
+    </CookiesProvider>
   </React.StrictMode>
 );
 
